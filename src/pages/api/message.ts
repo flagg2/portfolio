066b8c21fc936c5f804d9b0e2b41461b05f0d0b5
Samuel Wittlinger <samo.wittlinger@gmail.com>
2024-03-05 18:53:39 +0100

    fix: dont use node specific apis

--- src/pages/api/message.ts
+++ src/pages/api/message.ts
@@ -1,17 +1,8 @@
-import { TypeGrid } from "@flagg2/typegrid"
 import type { APIRoute } from "astro"
 
 export const prerender = false
 
 const { SG_API_KEY } = import.meta.env
-
-const tg = TypeGrid.createSync({
-   apiKey: SG_API_KEY,
-   sender: {
-      email: "form@tricks.sk",
-      name: "Tricks",
-   },
-})
 
 export const POST: APIRoute = async ({ request }) => {
    const body = await request.json()
@@ -21,23 +12,36 @@ export const POST: APIRoute = async ({ request }) => {
       return new Response("Bad request", { status: 400 })
    }
 
-   const result = await tg.sendPlain({
-      content: {
-         type: "text/plain",
-         value: `New Message! \n\n Email: ${email}\n\n${message}`,
-      },
-      subject: "New Contact Form Message",
-      personalizations: [
-         {
-            to: "samo.wittlinger@gmail.com",
+   const sendGridResponse = await fetch(
+      "https://api.sendgrid.com/v3/mail/send",
+      {
+         method: "POST",
+         headers: {
+            Authorization: `Bearer ${SG_API_KEY}`,
+            "Content-Type": "application/json",
          },
-      ],
-   })
+         body: JSON.stringify({
+            personalizations: [
+               {
+                  to: [{ email: "samo.wittlinger@gmail.com" }],
+                  subject: "New Contact Form Message",
+               },
+            ],
+            from: { email: "form@tricks.sk", name: "Tricks" },
+            content: [
+               {
+                  type: "text/plain",
+                  value: `New Message! \n\n Email: ${email}\n\n${message}`,
+               },
+            ],
+         }),
+      },
+   )
 
-   if (result.isOk()) {
+   if (sendGridResponse.ok) {
       return new Response("OK", { status: 200 })
    } else {
-      result.log()
-      return new Response(result.origin.message, { status: 500 })
+      const errorResult = await sendGridResponse.text() // Or .json() if response is JSON.
+      return new Response(errorResult, { status: 500 })
    }
 }
