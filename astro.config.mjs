44d202394606dba2501687b9d6d2adccef9ee051
Samuel Wittlinger <samo.wittlinger@gmail.com>
2024-03-05 11:32:49 +0100

    build: add cf adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,19 @@
-import { defineConfig } from "astro/config"
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react"
-import tailwind from "@astrojs/tailwind"
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-   integrations: [react(), tailwind()],
-   i18n: {
-      locales: ["en", "sk"],
-      defaultLocale: "en",
-      routing: {
-         prefixDefaultLocale: false,
-      },
-   },
-   output: "server",
-})
+  integrations: [react(), tailwind()],
+  i18n: {
+    locales: ["en", "sk"],
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  output: "server",
+  adapter: cloudflare()
+});
