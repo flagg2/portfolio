2d1214ed1df6c4fab0b7fca42644cffc915c4f7f
Samuel Wittlinger <samo.wittlinger@gmail.com>
2024-03-05 18:10:15 +0100

    tryfix: output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,6 @@ export default defineConfig({
          prefixDefaultLocale: false,
       },
    },
-   output: "hybrid",
+   output: "server",
    adapter: cloudflare({ imageService: "compile" }),
 })
