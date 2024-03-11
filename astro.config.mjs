bacf6cffdded3fb9f3d3046af93a097bb46743c4
Samuel Wittlinger <samo.wittlinger@gmail.com>
2024-03-11 19:35:46 +0100

    feat: try adding analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,24 @@
-import { defineConfig } from "astro/config"
-import react from "@astrojs/react"
-import tailwind from "@astrojs/tailwind"
-
-import cloudflare from "@astrojs/cloudflare"
+import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 // TODO: menu, finish translations, put something interesting at the bottom of the page
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
-   integrations: [react(), tailwind()],
-   i18n: {
-      locales: ["en", "sk"],
-      defaultLocale: "en",
-      routing: {
-         prefixDefaultLocale: false,
-      },
-   },
-   output: "hybrid",
-   adapter: cloudflare({ imageService: "compile" }),
-})
+  integrations: [react(), tailwind(), partytown()],
+  i18n: {
+    locales: ["en", "sk"],
+    defaultLocale: "en",
+    routing: {
+      prefixDefaultLocale: false
+    }
+  },
+  output: "hybrid",
+  adapter: cloudflare({
+    imageService: "compile"
+  })
+});
